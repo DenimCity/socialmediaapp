8f6853e78204c2be25d8aecd16e5ed7683df4602
jxa8hst <jean_altidor@homdepot.com>
2019-07-09 21:10:31 -0400

    signup function working

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,11 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const express = require('express');
-const app = express();
+const app = require('express')();
+
+const firebase = require('firebase');
+const firebaseConfig = require('./cred')
+
+firebase.initializeApp(firebaseConfig)
 admin.initializeApp();
 
 
@@ -18,7 +22,7 @@ app.get('/screams', async (req, res) => {
             });
             return res.json(screams)
       } catch (error) {
-            console.error(err)
+            console.error(error)
             return res.status(400).json({error: `Error retrieving data`, why: error.message})
       }
 })
@@ -36,10 +40,60 @@ app.post('/scream', async (req, res) => {
             const doc = await  admin.firestore().collection('screams').add(newScream)
             return res.json({message: `Document ${doc.id} created successfully`})
       } catch (error) {
-            console.error(err)
+            console.error(error)
             return res.status(500).json({error: `something went wrong`})
       }
 })
 
 
+app.post('/signup', async (req, res) => {
+
+      const newUser = {
+            email: req.body.email,
+            password: req.body.password,
+            confirmPassword: req.body.confirmPassword,
+            username: req.body.username,
+      }
+      
+  let token, userId;
+  db.doc(`/users/${newUser.username}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return res.status(400).json({ username: 'this username is already taken' });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        username: newUser.username,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId
+      };
+      return db.doc(`/users/${newUser.username}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === 'auth/email-already-in-use') {
+        return res.status(400).json({ email: 'Email is already is use' });
+      } else {
+        return res
+          .status(500)
+          .json({ general: 'Something went wrong, please try again' });
+      }
+    });
+
+})
 exports.api = functions.https.onRequest(app);
