2a0d1f160658d9a4d69ce2d3c308a114e82814e4
jxa8hst <jean_altidor@homdepot.com>
2019-07-09 22:32:23 -0400

    creted middleware

--- functions/index.js
+++ functions/index.js
@@ -27,8 +27,29 @@ app.get('/screams', async (req, res) => {
       }
 })
 
+async function authorization(req, res, next){
+      let idToken
+      if (req.headers.authorization && req.headers.authorization.includes('Bearer')){
+            idToken = req.headers.authorization.split('Bearer ')[1]
+      } else {
+            console.error('No token found')
+            return res.status(403).json({error: 'Unauthorized'})
+      }
 
-app.post('/scream', async (req, res) => {
+     try {
+       const decodedToken = await admin.auth().verifyIdToken(idToken);
+      console.log('decoded token',decodedToken)
+      req.user = decodedToken
+      const data = await db.collection('users').where('userID', '===', req.user.uid).limit(1).get()
+      req.user.username = data.docs[0].data().username;
+      return next()
+     } catch (error) {
+           console.error('Error while verifying token', err)
+           return res.status(403).json(error)
+     }
+}
+
+app.post('/scream',authorization, async (req, res) => {
       const { body, username } = req.body;
       const newScream = {
             body,
@@ -46,20 +67,39 @@ app.post('/scream', async (req, res) => {
 })
 
 
+
+
 app.post('/signup', async (req, res) => {
 
       const newUser = {
             email: req.body.email,
             password: req.body.password,
             confirmPassword: req.body.confirmPassword,
-            username: req.body.username,
+            username: req.user.username,
       }
 
       const {valid, errors} = validateRegisterInput(newUser);
         if (!valid) {
             return res.status(400).json({ message:'Input Validation Error', errors })
        }
+     /*  const user = await db.doc(`/users/${newUser.username}`).get()
+      if (user.exists){
+            return res.status(400).json({ username: 'this username is already taken' });
+      } else {
+          const data = await admin.auth().createUser({ newUser })
+          const userId = data.user.uid;
+          const token = await data.user.getIdToken();
+          const userCredentials = {
+                  username: newUser.username,
+                  email: newUser.email,
+                  createdAt: new Date().toISOString(),
+                  userId
+      };
 
+      db.doc(`/users/${newUser.username}`).set(userCredentials);
+      return res.status(201).json({ token });
+      }
+      */
   let token, userId;
   db.doc(`/users/${newUser.username}`)
     .get()
