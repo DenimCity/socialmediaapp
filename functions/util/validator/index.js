f7bae906bcfd1f4e6c7f42eb7c449b1757690d27
jxa8hst <jean_altidor@homdepot.com>
2019-07-10 20:03:45 -0400

    user credentials validator

--- functions/util/validator/index.js
+++ functions/util/validator/index.js
@@ -40,3 +40,22 @@ module.exports.validateLoginInput = (email, password) => {
             valid: Object.keys(errors).length < 1 
       }
 }
+
+const isEmpty = (string) => {
+  if (string.trim() === '') return true;
+  else return false;
+};
+exports.reduceUserDetails = (data) => {
+      let userDetails = {}
+      if (isEmpty(data.bio)) userDetails.bio = data.bio
+      if(isEmpty(data.website)){
+            if(data.website.trim().substring(0, 4) !== 'http'){
+                  userDetails.website = `http://${data.website.trim()}`
+            } else {
+                  userDetails.website = data.website.trim()
+            }
+      }
+      if (isEmpty(data.location)) userDetails.location = data.location
+
+      return userDetails;
+}
