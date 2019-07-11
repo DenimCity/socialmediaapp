f3084a519be49e993760c8ed20e1154bbd242c34
jxa8hst <jean_altidor@homdepot.com>
2019-07-10 21:22:01 -0400

    added scream/comment route

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,25 @@
 const functions = require('firebase-functions');
 const app = require('express')();
-const { getAllScreams, postOneScream} = require('./handlers/screams')
+const { getAllScreams, postOneScream, getScream, commentOnScream} = require('./handlers/screams')
 const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users')
 const { authCheck } = require('./util/middleware')
 
 // scream routes
 app.get('/screams', getAllScreams);
+app.get('/scream/:screamId', getScream)
+//protected scream routes
 app.post('/scream', authCheck, postOneScream);
-app.post('/user/image', authCheck, uploadImage);
-app.post('/user', authCheck, addUserDetails );
-app.get('/user', authCheck, getAuthenticatedUser)
+app.post('/scream/:screamId/comment', authCheck, commentOnScream)
+
+
 
 // user routes
 app.post('/signup', signup);
 app.post('/login', login);
+// protected user routes
+app.post('/user/image', authCheck, uploadImage);
+app.post('/user', authCheck, addUserDetails );
+app.get('/user', authCheck, getAuthenticatedUser)
 
 // images
 
