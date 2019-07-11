87262b3b84c583b2976eaee15842405d3373ca67
jxa8hst <jean_altidor@homdepot.com>
2019-07-10 20:04:19 -0400

    created user credentials route

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,20 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 const { getAllScreams, postOneScream} = require('./handlers/screams')
-const { signup, login, uploadImage } = require('./handlers/users')
+const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users')
 const { authCheck } = require('./util/middleware')
 
 // scream routes
 app.get('/screams', getAllScreams);
-app.post('/scream', authCheck, postOneScream)
+app.post('/scream', authCheck, postOneScream);
+app.post('/user/image', authCheck, uploadImage);
+app.post('/user', authCheck, addUserDetails );
+app.get('/user', authCheck, getAuthenticatedUser)
 
 // user routes
 app.post('/signup', signup);
 app.post('/login', login);
 
 // images
-app.post('/user/image', authCheck, uploadImage)
 
 exports.api = functions.https.onRequest(app);
