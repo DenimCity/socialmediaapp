dafff2767d8fd40364cc1013585117982bd54d26
jxa8hst <jean_altidor@homdepot.com>
2019-07-12 01:12:59 -0400

    created a root redcuer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,18 +1,10 @@
 import { combineReducers, createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
-
-import userReducer from './reducers/userReducer'
-import dataReducer from './reducers/dataReducer'
-import uiReducer from './reducers/uiReducer'
+import rootReducer from './reducers'
 
 const initialState = {}
 const middleware = [thunk];
 
-const reducers = combineReducers({
-      user: userReducer,
-      data: dataReducer,
-      UI: uiReducer
-})
 
 const composeEnhancers =
   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
@@ -20,6 +12,6 @@ const composeEnhancers =
     : compose;
 
 const enhancer = composeEnhancers(applyMiddleware(...middleware));
-const store = createStore(reducers, initialState, enhancer);
+const store = createStore(rootReducer, initialState, enhancer);
 
 export default store;
