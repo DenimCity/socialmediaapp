cb77e98d7ec43e5c53a76dbec1bc32b645a7cd8e
jxa8hst <jean_altidor@homdepot.com>
2019-07-12 01:01:53 -0400

    checking if user is authenticated on inital load

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,26 @@ import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import themeObject from './util/theme';
 import NavBar from './components/NavBar';
+import { SET_AUTHENTICATED } from './redux/types';
+import { logoutUser, getUserData } from './redux/actions/userActions';
+import jwtDecode from 'jwt-decode';
+import store from './redux/store'
+import axios from 'axios';
+import AuthRoute from './util/AuthRoute';
+
+
+const token = localStorage.FBIdToken
+if (token) {
+  const decodedToken = jwtDecode(token)
+  if (decodedToken.exp * 1000 < Date.now()){
+    store.dispatch(logoutUser)
+    window.location.href = '/login'
+  } else {
+   store.dispatch({ type: SET_AUTHENTICATED})
+   axios.defaults.headers.common['Authorization'] = token;
+   store.dispatch(getUserData())
+  }
+}
 
 const theme = createMuiTheme(themeObject);
 
@@ -17,8 +37,8 @@ function App() {
        <div className="container">
           <Switch>
           <Route exact path='/' component={Home}/>
-          <Route exact path='/login' component={Login}/>
-          <Route exact path='/signup' component={SignUp}/>
+          <AuthRoute exact path='/login' component={Login}/>
+          <AuthRoute exact path='/signup' component={SignUp}/>
         </Switch>
        </div>
       </Router>
